a028773cce42aa97b49a78626ea29d5e5a0faa2b
Gavi <gavitsra@gmail.com>
2023-12-01 08:21:45 +0100

    update code style

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,20 @@
     import { loadFull } from "tsparticles"
 
     const routes = {
-    "": SelectPage,
-    "/": SelectPage,
-    "/play/local/multiplayer": LocalMultiplayerPage,
-    "/play/local/singleplayer": SingleplayerPage,
-    "/play/online/waiting": WaitingPage,
-    "/play/online/join": JoinGamePage,
-    "/play/online/game": OnlineGamePage,
-    "/play/online/bot": OnlineSingleplayerPage
+        "": SelectPage,
+        "/": SelectPage,
+        "/play/local/multiplayer": LocalMultiplayerPage,
+        "/play/local/singleplayer": SingleplayerPage,
+        "/play/online/waiting": WaitingPage,
+        "/play/online/join": JoinGamePage,
+        "/play/online/game": OnlineGamePage,
+        "/play/online/bot": OnlineSingleplayerPage
     }
 
     const currentPath = ref(window.location.hash)
 
     window.addEventListener('hashchange', () => {
-    currentPath.value = window.location.hash
+        currentPath.value = window.location.hash
     })
 
     const currentView = computed(() => {
@@ -33,19 +33,14 @@
     })
 
     const particlesInit = async engine => {
-        await loadFull(engine);
-        //await loadSlim(engine);
-    };
-
-    const particlesLoaded = async container => {
-    };
+        await loadFull(engine)
+    }
 </script>
 
 <template>
     <vue-particles
         id="tsparticles"
         :particlesInit="particlesInit"
-        :particlesLoaded="particlesLoaded"
         url="/snow.json"
     />
   <component :is="currentView" />
